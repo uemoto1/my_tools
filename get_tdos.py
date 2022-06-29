addf168ba2fb27f26eb0f8cbd898741bd510a2a3
Mitsuharu UEMOTO <uemoto@eedept.kobe-u.ac.jp>
2022-06-29 11:25:43 +0900

    modified

--- get_tdos.py
+++ get_tdos.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import xml.etree.ElementTree as ET
 import numpy as np
-import re
 import sys
+import re
 
 tree = ET.parse('vasprun.xml')
 
@@ -17,46 +17,43 @@ print("# nedos=%d" % nedos)
 efermi = float(root.find(".//i[@name='efermi']").text)
 print("# efermi=%f" % efermi)
 
-data = np.zeros([nedos, 1+ispin])
+data = np.zeros([ispin, nedos, 2])
 
 element_set1 = root.find("calculation/dos/total/array/set")
 for element_set_spin in element_set1:
     comment = element_set_spin.attrib["comment"]
-    i = int(re.sub(r"spin\s*(\d+)", r"\1", comment))
+    i1 = int(re.sub(r"spin\s*(\d+)", r"\1", comment))
     for n, r in enumerate(element_set_spin):
         tmp = r.text.split()
-        data[n, 0] = float(tmp[0]) - efermi
-        data[n, i] = float(tmp[1])
+        data[i1-1, n, :] = float(tmp[0]), float(tmp[1])
 
-np.savetxt("tdos.txt", data, 
+        
+
+buff = np.zeros([nedos, ispin+1])
+buff[:, 0] = data[0, :, 0] - efermi
+buff[:, 1] = data[0, :, 1]
+buff[:, 2] = data[1, :, 1]
+np.savetxt("tdos.txt", buff, 
     header="Energy-EF[eV] DoS[1/eV]", fmt="%+12.6e")
 print("# Generated tdos.txt")
 
 
-element_field = root.findall("calculation/dos/partial/array/field")
 
-
-sys.exit(-1)
-
-header = "# Energy-EF"
-for item in element_field[1:]:
-    header += " " + item.text.strip()
-
-element_set1 = root.find("calculation/dos/partial/array/set")
-for element_set_ion in element_set1:
-    comment = element_set_ion.attrib["comment"]
-    iion = int(re.sub(r"ion\s*(\d+)", r"\1", comment))
-    for element_set_spin in element_set_ion:
-        comment = element_set_spin.attrib["comment"]
-        ispin = int(re.sub(r"spin\s*(\d+)", r"\1", comment))
-        with open("pdos%03d_spin%d.txt" % (iion, ispin), "w") as fh:
-            fh.write(header + "\n") 
-            for r in element_set_spin:
-                tmp = r.text.split()
-                energy = float(tmp[0]) - efermi
-                fh.write("%+12.6f" % energy)
-                for i in range(1, len(tmp)):
-                    fh.write(" %10.3e" % float(tmp[i]))
-                fh.write("\n")
-        print("# generated %s" % fh.name)
-
+if "-x" in sys.argv:
+    import matplotlib.pyplot as plt
+    xmin = np.amin(data[0, :, 0])
+    xmax = np.amax(data[0, :, 0])
+    ymax = np.amax(data[:, :, 1])
+    plt.plot(data[0, :, 0], +data[0, :, 1], "-k")
+    plt.xlim([xmin, xmax])
+    if ispin == 1:
+        plt.plot([0, 0], [0, ymax], "--k")
+        plt.ylim([0, ymax])
+    else: # ispin == 2
+        plt.plot(data[0, :, 0], -data[0, :, 1], "-k")
+        plt.plot([xmin, xmax], [0, 0], "--k")
+        plt.plot([0, 0], [-ymax, ymax], "--k")
+        plt.ylim([-ymax, ymax])
+    plt.xlabel("Energy [eV]")
+    plt.ylabel("DoS [1/eV]")
+    plt.show()
