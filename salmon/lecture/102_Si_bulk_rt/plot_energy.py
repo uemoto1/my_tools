ed5f710d21012270af1f1a44aeefda48b7a7e5fb
Mitsuharu UEMOTO <uemoto@eedept.kobe-u.ac.jp>
2024-12-12 17:37:52 +0900

    mod

---
+++ salmon/lecture/102_Si_bulk_rt/plot_energy.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+dat = np.loadtxt("Si_rt_energy.data")
+
+plt.figure(figsize=[5, 5])
+plt.plot(dat[:, 1-1]*0.0242, dat[:, 3-1]*27.2, "-b", label="E")
+plt.xlim([0, None])
+plt.ylim([None, None])
+plt.xlabel("Time $t$ [fsec]")
+plt.ylabel("Energy [eV]")
+plt.legend()
+plt.tight_layout()
+plt.savefig("plot_energy.png")
+plt.show()
