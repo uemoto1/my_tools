9e090ec93a7bc7792d6b230132774152c71f2622
Mitsuharu UEMOTO <uemoto@eedept.kobe-u.ac.jp>
2024-12-16 10:20:27 +0900

    modified

--- salmon/lecture/102_Si_bulk_rt/plot_energy.py
+++ salmon/lecture/102_Si_bulk_rt/plot_energy.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 dat = np.loadtxt("Si_rt_energy.data")
 
 plt.figure(figsize=[5, 5])
-plt.plot(dat[:, 1-1]*0.0242, dat[:, 3-1]*27.2, "-b", label="E")
+plt.plot(dat[:, 1-1]*0.0242, dat[:, 3-1]*27.2, "-blue", label="$E_ex$")
 plt.xlim([0, None])
 plt.ylim([0, None])
-plt.xlabel("Time $t$ [fsec]")
+plt.xlabel("Time [fsec]")
 plt.ylabel("Energy [eV]")
 plt.legend()
 plt.tight_layout()
