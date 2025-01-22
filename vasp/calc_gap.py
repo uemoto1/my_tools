038e76c6de0b4e79d616829148cd85e5666090ef
Mitsuharu UEMOTO <uemoto@eedept.kobe-u.ac.jp>
2025-01-22 14:16:16 +0900

    modified

--- vasp/calc_gap.py
+++ vasp/calc_gap.py
@@ -20,8 +20,8 @@ with open("EIGENVAL") as fh:
     print(f"# number of k = {nk}")
     print(f"# number of band = {nb}")
 
-    eig = np.empty([nk,nb])
-    occ = np.empty([nk,nb])
+    eig = np.zeros([nk,nb])
+    occ = np.zeros([nk,nb])
 
     for ik in range(nk):
         # Skip empty line
@@ -29,18 +29,32 @@ with open("EIGENVAL") as fh:
         # header
         line = fh.readline()
         k1, k2, k3, kw = np.fromstring(line, sep=" ", dtype=float)
-        print(f"#k {ik:3d} ({k1:6.2f} {k2:6.2f} {k3:6.2f}) {kw:12.3e}")
+        ik1 = ik + 1
+        print(f"# {ik1:3d} k=({k1:6.3f}, {k2:6.3f}, {k3:6.3f}) w={kw:12.3e}")
         for ib in range(nb):
             line = fh.readline()
             tmp = line.split()
             eig[ik, ib] = float(tmp[1])
             occ[ik, ib] = float(tmp[2])
-          
-ev_max = np.max(eig[occ > 0.5]) 
-ec_min = np.min(eig[occ < 0.5]) 
+        ivb = 0
+        icb = nb-1
+        for ib in range(nb):
+            if (eig[ik, ib] > eig[ik, ivb]) and occ[ik, ib] > 0.75:
+                ivb = ib
+            if (eig[ik, ib] < eig[ik, icb]) and occ[ik, ib] < 0.25:
+                icb = ib
+        evb = eig[ik, ivb]
+        ecb = eig[ik, icb]
+        ivb1 = ivb+1
+        icb1 = icb+1
+        print(f"# ... vb {ivb1:3d} {evb:8.3f} cb {icb1:3d} {ecb:8.3f}")
+
+
+ev_max = np.amax(eig[occ > 0.75]) 
+ec_min = np.amin(eig[occ < 0.25]) 
 e_gap = ec_min - ev_max
 
-print(f"# Maximal energy valence state: {ev_max:.6f}")
-print(f"# Minimal energy conduction band: {ec_min:.6f}")
+print(f"# Maximal energy valence: {ev_max:.6f}")
+print(f"# Minimal energy conduction: {ec_min:.6f}")
 print(f"# Energy gap: {e_gap:.6f}")
 
