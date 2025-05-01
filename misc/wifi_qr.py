40cd33975fd7eaba8dd60b63652651361a1cf9c5
Mitsuharu UEMOTO <uemoto@eedept.kobe-u.ac.jp>
2025-05-01 14:01:22 +0900

    modified

--- misc/wifi_qr.py
+++ misc/wifi_qr.py
@@ -1,16 +1,20 @@
 import argparse
 import qrcode
 from PIL import Image
+import os
+
+default_png = os.path.splitext(__file__)[0] + ".png"
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-s", "--ssid", required=True, help="SSID of the Wi-Fi network")
 ap.add_argument("-p", "--password", required=True, help="Password for the Wi-Fi network")
-ap.add_argument("-o", "--output", default="wifi_qr.png", help="Output filename for the QR code")
+ap.add_argument("-o", "--output", default=default_png, help="Output filename for the QR code")
 args = ap.parse_args()
 
 img = qrcode.make("WIFI:T:WPA;S:{SSID};P:{PASSWORD};H:false;;".format(
     SSID=args.ssid,
     PASSWORD=args.password,
 ))
+print(args.output)
 img.save(args.output)
 
