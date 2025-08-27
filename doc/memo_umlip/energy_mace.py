94e935fa9aea3e9ecc43db0e5138df11f45ce8af
Mitsuharu UEMOTO <uemoto@eedept.kobe-u.ac.jp>
2025-08-27 12:22:32 +0900

    fix

--- doc/memo_umlip/energy_mace.py
+++ doc/memo_umlip/energy_mace.py
@@ -1,5 +1,13 @@
-atoms = ase.io.read("ファイル名")
+import ase.io
+import mace.calculators 
 
+potential = mace.calculators.mace_mp(
+    model="medium-mpa-0",       # MACE-MPA-0
+    device="cuda",              # or "cpu"
+    default_dtype="float64",    # or float32
+)
+atoms = ase.io.read("POSCAR") 
+atoms.calc = potential
 
-
-
+energy = atoms.get_potential_energy()
+print("Total energy = %f [eV]" % energy)
