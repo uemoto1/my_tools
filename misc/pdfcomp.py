5d6118e146110b60fc6a5c9729d1167a68ceda3f
Mitsuharu UEMOTO <uemoto@eedept.kobe-u.ac.jp>
2025-12-03 12:13:58 +0900

    modified

--- misc/pdfcomp.py
+++ misc/pdfcomp.py
@@ -2,9 +2,13 @@
 import sys
 import os
 import argparse
+import shutil
 import subprocess
 
+
+
 def run1(file_input, file_output, dpi):
+    print(f"Convert {file_input} {file_output} ({dpi} dpi...)")
     subprocess.call([
         "gs",
         f"-sDEVICE=pdfwrite",
@@ -29,6 +33,7 @@ def run1(file_input, file_output, dpi):
     ])
 
 def run2(file_input, file_output, setting):
+    print(f"Convert {file_input} {file_output} ({setting})...")
     subprocess.call([
         f"gs",
         f"-sDEVICE=pdfwrite",
@@ -41,20 +46,39 @@ def run2(file_input, file_output, setting):
         file_input,
     ])
 
-
+def run3(file_input, file_output):
+    print(f"Convert {file_input} {file_output}...")
+    subprocess.call([
+        "inkscape",
+        file_input,
+        "--export-type=pdf",
+        f"--export-filename={file_output}",
+    ])
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("filelist", nargs="+")
     parser.add_argument("-d", "--density", default=96, type=int, help="Default 96 dpi")
     parser.add_argument("-s", "--setting", default="", help="screen (72dpi) | ebook (150dpi) | printer (300dpi)")
-    parser.add_argument("--suffix",  default="_min")
+    parser.add_argument("-O", "--overwrite", action="store_true", default=False, help="Overwrite input file")
     args = parser.parse_args()
 
     for file_input in args.filelist:
         if os.path.isfile(file_input):
-            name, ext = os.path.splitext(file_input)
-            file_output = "%s%s.pdf" % (name, args.suffix)
+            title, ext = os.path.splitext(file_input)
+            if ext.lower() == ".svg":
+                file_tmp = f"{title}.pdf"
+                run3(file_input, file_tmp)
+                file_input = file_tmp
+            elif ext.lower != ".pdf":
+                print(f"Invalid file extention: {file_input}!")
+            if args.overwrite:
+                file_backup = f"{title}_backup.pdf"
+                os.rename(file_input, file_backup)
+                file_output = file_input
+                file_input = file_backup
+            else:
+                file_output = f"{title}_min.pdf"
             if not args.setting:
                 run1(file_input, file_output, args.density)
             else:
--- misc/png2mp4.py
+++ misc/png2mp4.py
@@ -8,16 +8,15 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("filelist", nargs="+")
     parser.add_argument("--input", "-i", default="image%03d.png", help="Input filename (default: image%03d.png)")
-    parser.add_argument("--output", "-o", default="output.mp4", help="Output filename (default: image%03d.png)")
+    parser.add_argument("--output", "-o", default="output.mp4", help="Output filename (default: output.mp4)")
     parser.add_argument("--framerate",  "-r", default=12, type=int)
     args = parser.parse_args()
 
-
     subprocess.call([
         "ffmpeg",
-        "-framelate", str(args.framerate)
+        "-framerate", str(args.framerate),
         "-i", args.input,
-        "-vcodec", 
+        "-vcodec",
         "libx264",
         "-pix_fmt",
         "yuv420p",
