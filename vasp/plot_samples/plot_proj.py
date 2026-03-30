4b6fa30e6d47cce106e51fd8c0d2a8a3be5ecc0e
Mitsuharu UEMOTO <uemoto@eedept.kobe-u.ac.jp>
2026-03-30 13:18:09 +0900

    add plot_proj.py

---
+++ vasp/plot_samples/plot_proj.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+
+e_fermi =   -1.8209   # Modify Fermi energy
+e_range_min = None
+e_range_max = None
+ispin = 1 # 1 or 2
+# Specify target ion and orbits:
+# "s", "py", "pz", "px", "dyz", "dxy", "dz2", "dxz", "x2-y2"
+target_list = [
+    # (number, orbit),
+    (1, "px"),
+    (1, "py"),
+    (1, "s"),
+]
+color = "red"
+d_scale = 10.0
+d_step = 2
+output = "band_proj.png"
+
+################################################################################
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+band = np.loadtxt(f"band_spin{ispin}.txt")
+kpoint_labels = np.loadtxt("kpoint_labels.txt", dtype="str")
+
+# Calculate sum of projections over specified target orbitals ...
+tmp = []
+for i, orbit in target_list:
+    tmp.append(np.loadtxt(f"proj/ion{i:03d}_{orbit}_spin{ispin}.txt"))
+proj = sum(tmp)
+
+plt.figure(figsize=[5, 5], dpi=144)
+
+for i in range(band.shape[1]-1):
+    plt.plot(band[:, 0], band[:, i+1] - e_fermi, "-", color="gray")
+
+
+for ik in range(0, band.shape[0], d_step):
+    for i in range(band.shape[1]-1):
+        plt.plot(band[ik, 0], band[ik, i+1] - e_fermi, ".", ms=proj[ik, i]*d_scale, color=color)
+
+plt.xlim([band[0, 0], band[-1, 0]])
+plt.xlabel("Wavenumber $k$")
+
+if e_range_min is None:
+    e_range_min = min(band[:, 1]) - e_fermi - 1.0
+if e_range_max is None:
+    e_range_max = min(band[:, -1]) - e_fermi
+
+plt.ylim([e_range_min, e_range_max])
+plt.ylabel("Energy $E-E_F$ (eV)")
+
+k = np.array(kpoint_labels[:, 0], dtype=float)
+label = list(kpoint_labels[:, -1])
+plt.xticks(k, label)
+
+plt.grid()
+plt.tight_layout()
+plt.savefig(output)
+print(f"# Generate {output}")
+plt.show()
